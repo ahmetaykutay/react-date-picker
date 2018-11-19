3ecd1e276526ea7d3123c29f36fce50e65fed389
Ahmet Aykut Ay <ahmetaykutay@gmail.com>
2018-11-19 19:50:12 +0300

    first commit

---
+++ src/DatePicker.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+type DatePickerPropsType = {};
+
+
+class DatePicker extends Component<DatePickerPropsType> {
+  render(){
+    return (
+      <p>hello world</p>
+    );
+  }
+}
+
+export default DatePicker;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'reactDatePicker',
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [{
+      test: /(\.js$|\.jsx$)/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: [
+    {
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom"
+      }
+    },
+    {
+      react: {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ]
+}
