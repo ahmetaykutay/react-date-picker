80b2d0cb27316dd4a5be882e74fa1e05691f8680
Ahmet Aykut Ay <ahmetaykutay@gmail.com>
2018-11-27 03:54:29 +0300

    clear comments

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -31,33 +31,5 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    // alias: {
-    //   react: path.resolve('./node_modules/react'),
-    //   'react-dom': path.resolve('./node_modules/react-dom'),
-    //   'styled-components': path.resolve('./node_modules/styled-components'),
-    // },
   },
-  // externals: [
-  //   {
-  //     'styled-components': {
-  //       commonjs: 'styled-components',
-  //       commonjs2: 'styled-components',
-  //       amd: 'styled-components',
-  //     },
-  //     'react-dom': {
-  //       root: 'ReactDOM',
-  //       commonjs2: 'react-dom',
-  //       commonjs: 'react-dom',
-  //       amd: 'react-dom',
-  //     },
-  //   },
-  //   {
-  //     react: {
-  //       root: 'React',
-  //       commonjs2: 'react',
-  //       commonjs: 'react',
-  //       amd: 'react',
-  //     },
-  //   },
-  // ],
 };
